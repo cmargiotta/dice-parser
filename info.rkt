27137eb594bd0f7821d8fe4ec8c6f9a9da2cd9fb
car.margiotta <car.margiotta@icloud.com>
2020-04-21 13:27:56 +0200

    Let's roll!

--- info.rkt
+++ info.rkt
@@ -7,3 +7,6 @@
 (define pkg-desc "A parser for dice expressions like \"1d6-1d4+5\".")
 
 (define pkg-authors '("car.margiotta@icloud.com"))
+
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
---
+++ main.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(require "core.rkt")
+
+(provide roll)
+(provide simple-roll)
+(provide is-dice?)
+(provide is-simple-dice?)
