13eee3b58534bac735a2267f96ea8815e64b6e60
car.margiotta <car.margiotta@icloud.com>
2020-04-21 13:44:21 +0200

    Basic unit tests

--- main.rkt
+++ main.rkt
@@ -6,3 +6,16 @@
 (provide simple-roll)
 (provide is-dice?)
 (provide is-simple-dice?)
+
+(module+ test
+  (require rackunit))
+
+(module+ test
+  (define-values (str tot) (roll "2d20-1d6")))
+
+(module+ test
+  (check-true (list? (simple-roll "5d6")))
+  (check-= 5 (length (simple-roll "5d6")) 0))
+
+(module+ test
+  (check > tot 0))
