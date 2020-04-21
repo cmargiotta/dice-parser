e211edd2a97e38d72fd1b6618c799063425137f9
car.margiotta <car.margiotta@icloud.com>
2020-04-21 12:23:07 +0200

    Adapted package to standards

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define collection "dice-parser")
+
+(define version "0.0.1")
+
+(define pkg-desc "A parser for dice expressions like \"1d6-1d4+5\".")
+
+(define pkg-authors '("car.margiotta@icloud.com"))
