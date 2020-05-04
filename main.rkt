46a1b1fa1f2a74a3f2053adfc03171d19712c145
Carmine Margiotta <666cmoe666@gmail.com>
2020-05-04 19:54:59 +0000

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -78,11 +78,5 @@ TESTS
   (require rackunit))
 
 (module+ test
-  (define-values (str tot) (roll "2d20-1d6")))
-
-(module+ test
   (check-true (list? (simple-roll "5d6")))
   (check-= 5 (length (simple-roll "5d6")) 0))
-
-(module+ test
-  (check > tot 0))
